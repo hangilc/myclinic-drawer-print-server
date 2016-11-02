37751e30adb7f595d7da879d59056a6e747f8abb
hangilc <hangil@chang.jp>
2016-11-02 13:43:34 +0900

    initial commit

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 "use strict";
 
-var express = require("express");
-var bodyParser = require("body-parser");
-var cors = require("cors");
 var DrawerPrinter = require("myclinic-drawer-printer");
 
-function getConfig(config, key, defaultValue){
-	return (config && key in config) ? config[key] : defaultValue;
-}
+exports.staticDir = __dirname + "/static";
 
-exports.run = function(config){
-	var app = express();
-	app.use(bodyParser.urlencoded({extended: false}));
-	app.use(bodyParser.json());
-	app.use(express.static("static"));
-	app.use(cors());
+exports.initApp = function(app, config){
 	app.post("/print", function(req, res){
 		var pages = req.body.pages;
 		var setting = req.body.setting;
-		console.log("PRINT", setting);
 		if( setting ){
 			DrawerPrinter.readSetting(setting, function(err, result){
 				if( err ){
@@ -114,8 +103,4 @@ exports.run = function(config){
 			res.json(result);
 		})
 	});
-	var port = getConfig(config, "port", 8082);
-	app.listen(port, function(){
-		console.log("server (Print Server) listening to " + port);
-	})
 };
--- test-server.js
+++ test-server.js
@@ -1,4 +1,19 @@
 "use strict";
 
-var printApp = require("./index");
-printApp.run({port: 8082});
+var App = require("./index");
+var express = require("express");
+var bodyParser = require("body-parser");
+var config = {};
+
+var app = express();
+var sub = express();
+sub.use(bodyParser.urlencoded({extended: false}));
+sub.use(bodyParser.json());
+App.initApp(sub, config);
+app.use("/printer", sub);
+sub.use(express.static(App.staticDir));
+
+var port = 8082;
+app.listen(port, function(){
+	console.log("printer server listening to " + port);
+});
